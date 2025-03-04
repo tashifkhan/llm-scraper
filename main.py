6d634077d1244805a3e7f9da4eb417cb16d02c99
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-03-04 20:15:20 +0530

    refactored code

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import html2text
 import requests
 from bs4 import BeautifulSoup
-import os
 
-res = requests.get("https://pypi.org/project/beautifulsoup4/")
-soup = BeautifulSoup(res.content, 'html.parser')
-# print(soup.prettify())
-markdowntext = html2text.html2text(soup.prettify())
-# print(markdowntext)
+def return_md(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    markdowntext = html2text.html2text(soup.prettify())
+    return markdowntext
 
-with open("./test.md", "w+") as mdfl:
-    mdfl.write(markdowntext)
-    mdfl.close()
+def return_md2(url):
+    res = requests.get("https://r.jina.ai/"+url)
+    soup = BeautifulSoup(res.content)
+    return soup.prettify()
 
-res2 = requests.get("https://r.jina.ai/"+"https://pypi.org/project/beautifulsoup4/")
-# print(res2.content)
-soup2 = BeautifulSoup(res2.content)
-print(soup2.prettify())
-with open("./test2.md", "w+") as mdfl2:
-    mdfl2.write(soup2.prettify())
-    mdfl2.close()
+def write_file(filename, markdowntext):
+    with open(filename, "w+") as ft:
+        ft.write(markdowntext)
+        ft.close()
+
+def main():
+    write_file("test2.md", return_md("https://portfolio.tashif.codes/"))
+    write_file("test3.md", return_md2("https://portfolio.tashif.codes/"))
+
+if __name__ == "__main__":
+    main()
 
