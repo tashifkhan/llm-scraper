44b129b29960acbc0110a25906a6954ada8057b2
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-03-04 21:10:03 +0530

    intialised genai

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
 import html2text
 import requests
+from google import genai
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+print(api_key)
 
 def return_md(url):
     res = requests.get(url)
@@ -17,6 +25,13 @@ def write_file(filename, markdowntext):
     with open(filename, "w+") as ft:
         ft.write(markdowntext)
         ft.close()
+
+def llm_prompt_response(prompt):
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents="Explain how AI works"
+    )
+    print(response.text)
 
 def main():
     write_file("test2.md", return_md("https://portfolio.tashif.codes/"))
