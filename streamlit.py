f1f32b0479b16d78224365c7da0539f9442e2e77
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-03-04 23:51:38 +0530

    minor eror fix

--- streamlit.py
+++ streamlit.py
@@ -9,21 +9,18 @@ if st.button("Scrape Website"):
         st.write("Scraping the website...")
 
         cleaned_content = return_awaited_md(url)
-
         st.session_state.dom_content = cleaned_content
 
-        # Display the DOM content in an expandable text box
         with st.expander("View DOM Content"):
             st.text_area("DOM Content", cleaned_content, height=300)
 
 
-# Step 2: Ask Questions About the DOM Content
 if "dom_content" in st.session_state:
     parse_description = st.text_area("Describe what you want to parse")
 
     if st.button("Parse Content"):
         if parse_description:
             st.write("Parsing the content...")
-            prompt = prompt_generator(cleaned_content, parse_description)
+            prompt = prompt_generator(st.session_state.dom_content, parse_description)
             response = llm_prompt_response(prompt)
             st.write(response)
