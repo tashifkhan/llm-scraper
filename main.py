962ce066e38915025f2c98203447dbb2096aefe9
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-03-04 21:18:27 +0530

    added prompt generator

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("GOOGLE_API_KEY")
 
-print(api_key)
-
 def return_md(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
@@ -26,16 +24,33 @@ def write_file(filename, markdowntext):
         ft.write(markdowntext)
         ft.close()
 
+def prompt_generator(scrapped_content, prompt):
+    return (
+    f"""
+        Website Scapped Content:
+        {scrapped_content}
+
+        based on the sceapped content answer the follwing question and provide answers following these guidlines:
+            "1. **Extract Information:** Only extract the information that directly matches the provided scarpped content. "
+            "2. **No Extra Content:** Do not include any additional text, comments, or explanations in your response. "
+            "3. **Empty Response:** If no information matches the description, return an empty string ('')."
+            "4. **Direct Data Only:** Your output should contain only the data that is explicitly requested, with no other text."
+
+        question: {prompt}
+
+    """)
+
 def llm_prompt_response(prompt):
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.0-flash", contents="Explain how AI works"
+        model="gemini-2.0-flash", contents=prompt
     )
     print(response.text)
 
 def main():
-    write_file("test2.md", return_md("https://portfolio.tashif.codes/"))
-    write_file("test3.md", return_awaited_md("https://portfolio.tashif.codes/"))
+    # write_file("test2.md", return_md("https://portfolio.tashif.codes/"))
+    # write_file("test3.md", return_awaited_md("https://portfolio.tashif.codes/"))
+    llm_prompt_response("Hello what all can you do?")
 
 if __name__ == "__main__":
     main()
